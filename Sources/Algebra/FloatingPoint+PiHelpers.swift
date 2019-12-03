44f634e89fb55bd75b8a4426df18a212cd01b04a
CypherPoet <CypherPoet@gmail.com>
2019-12-02 21:39:00 -0600

    Add Pi helpers for floating-point types

---
+++ Sources/Algebra/FloatingPoint+PiHelpers.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+
+extension Double {
+    static let twoPi = pi * 2.0
+    static let halfPi = pi / 2.0
+    static let quarterPi = pi / 4.0
+}
+
+
+extension Float {
+    static let twoPi = pi * 2.0
+    static let halfPi = pi / 2.0
+    static let quarterPi = pi / 4.0
+}
